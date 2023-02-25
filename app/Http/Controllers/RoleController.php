bef5cea12a2cf9d2fc3b671fb646ecf7d04c98eb
salimarizi <salim9asmp1pdl@gmail.com>
2023-02-25 17:57:55 +0700

    role feature

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+use App\Models\Role;
+use ResponseHelper;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            return ResponseHelper::success(Role::all());
+        } catch (Exception $e) {
+            return ResponseHelper::failed($e, 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $role = Role::find($id);
+            if (!$role) {
+                return ResponseHelper::failed("Data Not found", 404);
+            } else {
+                return ResponseHelper::success($role);
+            }
+        } catch (Exception $e) {
+            return ResponseHelper::failed($e, 500);
+        }
+    }
+}
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = ['normal', 'manager', 'admin'];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+    }
+}
